0030d400d055953e5f36df33c0d6a996640da621
Faraaz Khan <faraaz@rationalizeit.us>
2011-12-11 16:27:55 -0500

    Adding capistrano configuration for DH

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ gem 'xml-simple',     '1.0.12', :require => 'xmlsimple'
 # this is only needed by jammit, but we're pinning at 0.9.4 because 0.9.5 breaks
 gem 'yui-compressor', '0.9.4'
 gem 'taps'
+gem 'capistrano'
 
 group :mysql do
   gem 'mysql',        '2.8.1'
---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+set :application, "lms"
+set :repository,  "git@github.com:faraazkhan/canvas.git"
+
+set :scm, "git" 
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :deploy_to, "/var/www"
+
+role :web, "lms.rationalizeitconsulting.com"
+role :app, "lms.rationalizeitconsulting.com"
+role :db,  "mysql.rationalizeitconsulting.com", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+set :user, "faraazkhan"
+set :use_sudo, false
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+ namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
