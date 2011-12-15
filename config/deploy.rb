038ecef8cc12153c5f0307a15c6606a8c4d0703b
Faraaz Khan <faraaz@rationalizeit.us>
2011-12-15 03:07:11 -0500

    new ami

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,17 +6,15 @@ set :scm, "git"
 set :scm_passphrase, "faraazkhan"
 set :branch, "master"
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-set :deploy_to, "~/rationalizeit/lms.rationalizeitconsulting.com/current/var/www/lms.rationalizeitconsulting.com"
-set :location, "ec2-50-17-45-242.compute-1.amazonaws.com"
+set :deploy_to, "~/bitnami/rationalizeit/lms.rationalizeitconsulting.com/current/var/www/lms.rationalizeitconsulting.com"
+set :location, "ec2-107-22-40-195.compute-1.amazonaws.com"
 
 role :web, location 
 role :app, location 
 role :db,  location 
 #role :db,  "your slave db-server here"
-set :user, "root"
-ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "rationalizeit.pem")]
+set :user, "bitnami"
 set :use_sudo, false
-set :default_environment, {'PATH' => "'/usr/lib/ruby/gems/1.8/bin//bundle:/home/faraazkhan/.gems/bin:/usr/lib/ruby/gems/1.8/bin/:/usr/local/bin:/usr/bin:/bin:/usr/bin/X11:/usr/games'"}
 default_run_options[:pty] = true
 
 # if you're still using the script/reaper helper you will need
